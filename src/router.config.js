c94afd85fd77ca07d29c062a5ec74246c5324c24
Nik Atlas <nikatlas@gmail.com>
2017-12-11 12:41:38 +0200

    net https

--- src/router.config.js
+++ src/router.config.js
@@ -1,7 +1,7 @@
 import {UIRouterReact, servicesPlugin, pushStateLocationPlugin} from '@uirouter/react';
 
 //add visualizer
-//import {visualizer} from '@uirouter/visualizer';
+import {visualizer} from '@uirouter/visualizer';
 
 import appStates from './states';
 
@@ -21,4 +21,4 @@ router.urlService.rules.initial({ state: 'login' });
 //router.start();
 
 // Setup the state visualizer
-//visualizer(router);
+visualizer(router);
