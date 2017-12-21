f9f23023167f25967777f1d634e717619b0d8ad8
Nikatlas <nikatlas@gmail.com>
2017-12-21 20:08:40 +0200

    Map view update

--- src/Components/MapView.jsx
+++ src/Components/MapView.jsx
@@ -1,39 +1,17 @@
 import React, { Component } from 'react';
 import Net from '../helpers/net';
 
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import './MapView.css';
+
 import GoogleMapReact from 'google-map-react';
 
-class MyMapComponent extends Component{
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      markers: []
-    };
-  }
-  componentWillReceiveProps(props) {
-    this.setState({
-      markers: props.markers
-    });
-  }
+class MyMarker extends Component {
 
   render() {
-    return (
-    <GoogleMap
-
-      defaultZoom={12}
-      defaultCenter={{lat: 40.63178, lng: 22.95151}}
-    >
-      {this.state.markers.map((coords, index) => {
-        <Marker position={coords} />
-      })}
-    </GoogleMap>
-    );
+    const pos = 'mymarker-' + (this.props.data.position || 0);
+    return <div className={`mymarker ${pos}`}>M</div>;
   }
 }
-const MyGoogleMap = withScriptjs(withGoogleMap(MyMapComponent));
-const MyMarker = ({}) => <div>M</div>;
 class MapView extends Component {
 
   constructor(props){
@@ -48,14 +26,21 @@ class MapView extends Component {
   castToCoords(marker) { 
     let res = {
       lng: marker.coords.longitude,
-      lat: marker.coords.latitude
+      lat: marker.coords.latitude,
+      position: marker.position
     }
     return res;
   }
   createMarkers = (data) => {
     let marks = [];
+    let groups = [];
     for(let i=0;i<data.length;i++){
-      marks.push(this.castToCoords(JSON.parse(data[i].Value)));
+      let obj = JSON.parse(data[i].Value);
+      if(groups[obj.user_id] > 8)
+        continue;
+      groups[obj.user_id] = (groups[obj.user_id] + 1 )|| 0;
+      obj.position = groups[obj.user_id];
+      marks.push(this.castToCoords(obj));
     }
     return marks;
   }
@@ -80,13 +65,14 @@ class MapView extends Component {
         >
               {this.state.markers.map((marker, i) =>{
                 return(
-                  <MyMarker key={i}
+                  <MyMarker key={''+i+marker.lat + marker.lng}
                     lat={marker.lat}
                     lng={marker.lng}
+                    data={marker}
                   />
 
                 )
-              })}      
+              })}
         </GoogleMapReact>
       </div>
     );
